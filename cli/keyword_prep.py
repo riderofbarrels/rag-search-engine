cae3efe9b4eda4c3834fc7c197936b21b8ab4c38
riderofbarrels <garbage@learningstuff.com>
2026-04-09 01:58:48 -0500

    finished lesson 1

---
+++ cli/keyword_prep.py
@@ -0,0 +1,17 @@
+import string
+from tracemalloc import stop
+
+from nltk.stem import PorterStemmer
+
+
+def prep_keywords(keywords):
+    punct_map = str.maketrans("", "", string.punctuation)
+    clean_list = keywords.lower().translate(punct_map).split()
+    return clean_list
+
+
+def remove_stopwords(keywords):
+    with open("data/stopwords.txt", "r") as f:
+        stopwords = f.read().splitlines()
+
+    return list(set(keywords) - set(stopwords))
